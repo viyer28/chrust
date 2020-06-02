5b4c2bbf8044ca8eb6512a662d2bd1845a0d2e6f
Sam Sendelbach <sbsends@gmail.com>
2020-06-02 14:59:44 -0500

    added CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 extern crate zmq;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+pub struct CLI {
+    #[structopt(long)]
+    node_name: String,
+
+    #[structopt(long)]
+    pub_endpoint: String,
+
+    #[structopt(long)]
+    router_endpoint: String,
+
+    #[structopt(long)]
+    peer: Option<Vec<String>>,
+}
+
 
 fn main() {
+
+    let cli_options = CLI::from_args();
+    println!("{:#?}", cli_options);
+
     let ctx = zmq::Context::new();
 
-    let socket = ctx.socket(zmq::REQ).unwrap();
+    let socket = ctx.socket(zmq::PUB).unwrap();
     socket.connect("tcp://127.0.0.1:1234").unwrap();
     socket.send("hello world!", 0).unwrap();
 }
